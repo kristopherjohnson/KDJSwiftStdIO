12bc9cf59c7d18d370d02bfd4318a09ba46adde4
Kristopher Johnson <kris@kristopherjohnson.net>
2014-07-19 13:00:33 -0400

    Initial Commit

---
+++ KDJSwiftStdIO/KDJSwiftStdIO.h
@@ -0,0 +1,19 @@
+//
+//  KDJSwiftStdIO.h
+//  KDJSwiftStdIO
+//
+//  Created by Kristopher Johnson on 7/19/14.
+//  Copyright (c) 2014 Kristopher Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KDJSwiftStdIO.
+FOUNDATION_EXPORT double KDJSwiftStdIOVersionNumber;
+
+//! Project version string for KDJSwiftStdIO.
+FOUNDATION_EXPORT const unsigned char KDJSwiftStdIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KDJSwiftStdIO/PublicHeader.h>
+
+
